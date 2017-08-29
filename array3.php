bd043954eb6bfa2529c526f8fd5d0386c9720ae1
unknown <windikadm3@gmail.com>
2017-08-29 11:20:48 +0700

    latihan_array

---
+++ array3.php
@@ -0,0 +1,8 @@
+<?php
+$age = array("Dika"=>"18", "Ben"=>"18", "Anggi"=>"19");
+echo "Dika " . $age['Dika'] . " tahun";
+echo "<br>";
+echo "Ben " . $age['Ben'] . " tahun";
+echo "<br>";
+echo "Anggi " . $age['Ben'] . " tahun";
+?>
